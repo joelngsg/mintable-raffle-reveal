c8e0fddef202fd42f499157e2afdb6fa5e40fa8b
Ubuntu <ubuntu@ip-172-31-19-227.ap-southeast-1.compute.internal>
2022-05-09 18:53:01 +0800

    updating codebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import RaffleReveal from './routes/raffleReveal/raffleRevealView'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
   return (
-    <div>
-      <RaffleReveal  />
-    </div>
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={
+            <RaffleReveal  />
+          }/>
+        </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+const Header = () =>
+{
+	return (
+		<div>
+			Header Placeholder
+		</div>
+		)	
+}
+
+export default Header;
